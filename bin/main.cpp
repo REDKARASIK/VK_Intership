6de654e7af5e7a5cd3b9c8d0b8c45e0c5ef66324
redkarasik <redkarasik@yandex.ru>
2025-06-16 21:00:53 +0300

    add main simple program

--- bin/main.cpp
+++ bin/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
-#include "lib/metric/metric.h"
 #include "lib/metric_collector/metric_collector.h"
+#include "lib/metric/metric.h"
+#include <chrono>
+#include <thread>
 
 int main(int argc, char** argv) {
-    Metric<float> cpu("CPU", 0.97, "234");
-    std::cout << cpu.get_datetime();
+    MetricCollector collector{32};
+    collector.start("metrics.log");
+    collector.push<Metric<double>>("CPU", 0.72);
+    collector.push<Metric<int>>("HTTP requests RPS", 55);
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+    collector.push<Metric<double>>("CPU", 0.81);
+    collector.push<Metric<int>>("HTTP requests RPS", 49);
+    collector.stop(); 
     return 0;
 }
